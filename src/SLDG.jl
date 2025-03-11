ae94d4a04e17bc0c4d4975a35b5ae32f3d54783f
Krigo-Liu <liuyuhailiuyuhao@gmail.com>
2025-03-11 01:58:14 -0500

    Test

--- src/SLDG.jl
+++ src/SLDG.jl
@@ -2,13 +2,17 @@ module SLDG
 
 # greet() = print("Hello World!")
 include("SLDG1d.jl")
+include("test1d.jl")
+
 using .SLDG1d
 
 export SimulationParameters, SimulationState 
 export Vertex, EulerianElement, Segment, UpstreamElement
 export sldg1d
 
-include("../test/test1d.jl")
-
+# 动态加载 test1d.jl
+const test1d = let file = joinpath(dirname(@__FILE__), "..", "test", "test1d.jl")
+    eval(Meta.parse(read(file, String)))
+end
 
 end # module SLDG
